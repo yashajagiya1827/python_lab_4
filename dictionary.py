59ffa744c6cd647c823a7f8cfc09816f044b9f7c
yashajagiya1827 <bca2022yash1827@tnraocollege.org>
2024-08-12 00:55:49 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+emp = {
+    'id': 1,
+    'name': 'Yash Ajagiya',
+    'designation': 'CEO',
+    'salary': 9000000
+}
+
+print("Dictionary:",emp)
+
+del emp['designation']
+print("\nDeleting Designation:",emp)
+
+emp['name'] = 'Yash Ajagiya'
+print("\nUpdating 'name':",emp)
